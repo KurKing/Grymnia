48ea710e482da2f4d0dfd266be460131b5a23c53
NSString <lyosha.kurkin@gmail.com>
2026-07-28 22:17:13 +0300

    ios project

---
+++ Grymnia/GrymniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrymniaApp.swift
+//  Grymnia
+//
+//  Created by Oleksii Kurkin on 28.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct GrymniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
